8774ccbc1e27ebab408adb7847a5b0f8a84b31ba
Konstantin S. <havendv@gmail.com>
2026-06-26 10:44:33 +0400

    feat: Updated OpenAPI spec (#95)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/cli/Composio.CLI/Commands/FilesApiGroupCommand.g.cs
+++ src/cli/Composio.CLI/Commands/FilesApiGroupCommand.g.cs
@@ -9,7 +9,6 @@ internal static class FilesApiGroupCommand
     public static Command Create()
     {
         var command = new Command(@"files", @"Files endpoint commands.");
-                         command.Subcommands.Add(FilesGetFilesListCommandApiCommand.Create());
                          command.Subcommands.Add(FilesPostFilesUploadRequestCommandApiCommand.Create());
         return command;
     }
