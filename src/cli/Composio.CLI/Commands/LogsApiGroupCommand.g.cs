b324a414546c6cf1736f74acc6c204646d488cce
Konstantin S. <havendv@gmail.com>
2026-07-03 10:22:19 +0400

    feat: Updated OpenAPI spec (#99)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/cli/Composio.CLI/Commands/LogsApiGroupCommand.g.cs
+++ src/cli/Composio.CLI/Commands/LogsApiGroupCommand.g.cs
@@ -9,7 +9,6 @@ internal static class LogsApiGroupCommand
     public static Command Create()
     {
         var command = new Command(@"logs", @"Logs endpoint commands.");
-                         command.Subcommands.Add(LogsGetInternalActionExecutionFieldsCommandApiCommand.Create());
                          command.Subcommands.Add(LogsGetInternalActionExecutionLogByIdCommandApiCommand.Create());
                          command.Subcommands.Add(LogsGetInternalTriggerLogByIdCommandApiCommand.Create());
                          command.Subcommands.Add(LogsPostInternalActionExecutionLogsCommandApiCommand.Create());
